a011d406ecc2d596560a8cef4da8b22edb217c18
Hieu Tran <hmtran.ae@gmail.com>
2019-01-27 17:37:21 -0500

    initial todo setup with state and hooked up with onChange and onKeyPress

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    todos: [],
+    inputVal: ''
+  }
+
+  componentDidMount = () => {}
+
+  componentDidUpdate = () => {}
+
+  onSubmit = e => {
+    if (e.key === 'Enter') {
+      this.setState({
+        todos: [...this.state.todos, e.target.value],
+        inputVal: ''
+      })
+    }
+  }
+
+  onChange = e => {
+    this.setState({
+      inputVal: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="ui center aligned container">
+        <h1
+          style={{ opacity: '0.3', fontSize: '100px' }}
+          className="ui red center aligned block header"
+        >
+          todos
+        </h1>
+        <div className="ui massive form">
+          <div className="field">
+            <input
+              onChange={this.onChange}
+              onKeyPress={this.onSubmit}
+              value={this.state.inputVal}
+              type="text"
+              placeholder="What needs to be done?"
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
