92bade85d16593cb3af1dd3af2d008b20fdef491
Hieu Tran <hmtran.ae@gmail.com>
2019-01-27 17:51:35 -0500

    added comments in app.js (on state, localstorage and form callback func)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  // todos array holds all todo items
+  // inputVal is todo text currently being inputted
   state = {
     todos: [],
     inputVal: ''
@@ -10,18 +12,22 @@ export default class App extends Component {
 
   componentDidUpdate = () => {}
 
+  // submit only if e.key is enter key and not empty input value
   onSubmit = e => {
     if (e.key === 'Enter' && this.state.inputVal) {
       this.setState(
         {
           todos: [...this.state.todos, e.target.value],
+          // using controlled inputs, reset inputVal to be empty
           inputVal: ''
         },
+        // callback function using localstorage setter to update its todo list
         () => localStorage.setItem('todos', JSON.stringify(this.state.todos))
       )
     }
   }
 
+  // updates the controlled input with onChange
   onChange = e => {
     this.setState({
       inputVal: e.target.value
