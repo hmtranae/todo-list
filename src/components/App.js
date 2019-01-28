5ec8fc81100263b84e5d2560893e36516f0d3b2d
Hieu Tran <hmtran.ae@gmail.com>
2019-01-27 19:49:27 -0500

    fixed error when localStorage is empty, changed todo state into an array of objects

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,31 @@ export default class App extends Component {
   // todos array holds all todo items
   // inputVal is todo text currently being inputted
   state = {
-    // todos array will be grabbed from localStorage getItem
-    todos: JSON.parse(localStorage.getItem('todos')),
+    // initial value of todos is empty array
+    todos: [],
     inputVal: '',
     isHovering: false
   }
 
-  componentDidMount = () => {}
+  componentDidMount = () => {
+    // todos array will be grabbed from localStorage getItem if localStorage is not empty
+    if (localStorage.length) {
+      this.setState({
+        todos: JSON.parse(localStorage.getItem('todos'))
+      })
+    }
+  }
 
   componentDidUpdate = () => {}
 
   // submit only if e.key is enter key and not empty input value
   onSubmit = e => {
     if (e.key === 'Enter' && this.state.inputVal) {
+      const todo = {value: e.target.value, isHovering: false}
       this.setState(
         {
-          todos: [...this.state.todos, e.target.value], // using controlled inputs, reset inputVal to be empty
+          todos: [...this.state.todos, todo],
+          // using controlled inputs, reset inputVal to be empty
           inputVal: ''
         }, // callback function using localstorage setter to update its todo list
         () => localStorage.setItem('todos', JSON.stringify(this.state.todos))
@@ -33,7 +42,9 @@ export default class App extends Component {
   }
 
   // handleMouseHover returns opposite of isHovering
-  handleMouseHover = () => {
+  // i gives the id of that todo
+  handleMouseHover = i => {
+    console.log(i)
     this.setState({
       isHovering: !this.state.isHovering
     })
@@ -66,17 +77,19 @@ export default class App extends Component {
         </div>
         {/* map through todos list and only show if length of todo is greater than 0 */}
         {this.state.todos.length ? (
-          <div
-            className="ui stacked segments" // show delete icon when mouse over that todo
-            onMouseEnter={this.handleMouseHover}
-            onMouseLeave={this.handleMouseHover}
-          >
-            {this.state.todos.map(todo => {
+          <div className="ui stacked segments">
+            {this.state.todos.map((todo, i) => {
               return (
-                <div key={todo.toString()} className="ui left aligned secondary segment">
+                <div
+                  key={i}
+                  className="ui left aligned secondary segment"
+                  // show delete icon when mouse over that todo
+                  onMouseEnter={this.handleMouseHover.bind(null, i)}
+                  onMouseLeave={this.handleMouseHover}
+                >
                   <p style={{ paddingLeft: '10px', fontSize: '20px' }}>
-                    {todo}
-                    {this.state.isHovering ? (
+                    {todo.value}
+                    {todo.isHovering ? (
                       <button
                         onClick={this.deleteTodo}
                         className="ui red basic right floated icon button"
